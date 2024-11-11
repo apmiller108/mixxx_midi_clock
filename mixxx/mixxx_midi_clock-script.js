77914c5e00256561f22acf97a85b67e4101a2104
Alex Miller <apmiller108@yahoo.com>
2024-11-10 21:54:51 -0600

    Controller script fixes

--- mixxx/mixxx_midi_clock-script.js
+++ mixxx/mixxx_midi_clock-script.js
@@ -14,17 +14,53 @@
 
 const midiChannel = 11;
 
-class MixxxMIDIClock {
-  timer = 0;
-  decks = [
-    new Deck('[Channel1]'),
-    new Deck('[Channel2]'),
-    new Deck('[Channel3]'),
-    new Deck('[Channel4]')
-  ];
+class Deck {
+  constructor(group) {
+    this.group = group;
+    this.playLatched = engine.getValue(group, 'play_latched');
+    engine.makeConnection(group, 'play_latched', (value) => {
+      // Set to 1 when deck is playing, but not when previewing.
+      this.playLatched = value;
+    });
+  }
 
-  static get midiChannel() {
-    return midiChannel;
+  // A float, precision beyond 2 decimal places.
+  bpm() {
+    return engine.getValue(this.group, "bpm");
+  }
+
+  // 0 = sync lock disabled, 1 = sync follower, 2 = sync leader.
+  syncMode() {
+    return engine.getValue(this.group, 'sync_mode');
+  }
+
+  // A value between 0..1. Relative position of the play marker between beat markers.
+  beatDistance() {
+    return engine.getValue(this.group, "beat_distance");
+  }
+
+  isSyncLeader() {
+    return this.syncMode() == 2;
+  }
+
+  isSyncFollower() {
+    return this.syncMode() == 1;
+  }
+
+  isPlaying() {
+    return this.playLatched == 1;
+  }
+}
+
+var mixxxMIDIClock = new class MixxxMIDIClock {
+  constructor() {
+    this.timer = 0;
+    this.decks = [
+      new Deck('[Channel1]'),
+      new Deck('[Channel2]'),
+      new Deck('[Channel3]'),
+      new Deck('[Channel4]')
+    ];
   }
 
   init() {
@@ -35,6 +71,14 @@ class MixxxMIDIClock {
     const syncLeader = this.findSyncLeader();
     let syncFollower;
     let anyPlayingDeck;
+
+    this.decks.forEach((d) => {
+      console.log(d.group);
+      console.log(d.syncMode());
+      console.log(d.bpm());
+      console.log(d.isPlaying());
+      console.log(d.beatDistance());
+    });
 
     // If there is a sync leader and at least one deck is playing send Midi data
     // with BPM and playing deck beat distance over three messages.
@@ -49,12 +93,16 @@ class MixxxMIDIClock {
       }
       if (beatDistance != undefined) {
         const bpmParts = syncLeader.bpm().toFixed(2).toString().split('.').map(v => parseInt(v));
-        const bpmWhole = Math.max(0, Math.min(127, bpmParts[0] - 60)); // supports BPM in the range of 60..187
+        // supports BPM in the range of 60..187
+        const bpmWhole = Math.max(0, Math.min(127, bpmParts[0] - 60));
         const bpmFractional = bpmParts[1];
 
-        midi.sendShortMsg(0x8F + MixxxMIDIClock.midiChannel, 0x34, bpmWhole); // Note E (52) On
-        midi.sendShortMsg(0x8F + MixxxMIDIClock.midiChannel, 0x35, bpmFractional); // note F (53) On
-        midi.sendShortMsg(0x8F + MixxxMIDIClock.midiChannel, 0x32, Math.round(beatDistance * 127)); // Note D (50) On
+        // Note E (52) On
+        midi.sendShortMsg(0x8F + midiChannel, 0x34, bpmWhole);
+        // note F (53) On
+        midi.sendShortMsg(0x8F + midiChannel, 0x35, bpmFractional);
+        // Note D (50) On
+        midi.sendShortMsg(0x8F + midiChannel, 0x32, Math.round(beatDistance * 127));
       } else {
         this.sendOffMessage();
       }
@@ -73,7 +121,7 @@ class MixxxMIDIClock {
 
   sendOffMessage() {
     // Note D (50) Off message with value 0
-    midi.sendShortMsg(0x7F + MixxxMIDIClock.midiChannel, 0x32, 0x0);
+    midi.sendShortMsg(0x7F + midiChannel, 0x32, 0x0);
   }
 
   shutdown() {
@@ -83,44 +131,3 @@ class MixxxMIDIClock {
   }
 }
 
-class Deck {
-  group;
-  syncMode;
-  playLatched;
-
-  constructor(group) {
-    this.group = group;
-    engine.makeConnection(group, 'sync_mode', (value) => {
-      // 0 = sync lock disabled, 1 = sync follower, 2 = sync leader.
-      this.syncMode = value;
-    });
-    engine.makeConnection(group, 'play_latched', (value) => {
-      // Set to 1 when deck is playing, but not when previewing.
-      this.playLatched = value;
-    });
-  }
-
-  // A float, precision beyond 2 decimal places.
-  bpm() {
-    return engine.getValue(this.group, "bpm");
-  }
-
-  // A value between 0..1. Relative position of the play marker between beat markers.
-  beatDistance() {
-    return engine.getValue(this.group, "beat_distance");
-  }
-
-  isSyncLeader() {
-    return this.syncMode == 2;
-  }
-
-  isSyncFollower() {
-    return this.syncMode == 1;
-  }
-
-  isPlaying() {
-    return this.playLatched == 1;
-  }
-}
-
-const mixxxMIDIClock = new MixxxMIDIClock();
